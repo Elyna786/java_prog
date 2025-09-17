213f4d2e8ec8b6499310c86db17738283567b115
Elyna786 <135322645+Elyna786@users.noreply.github.com>
2025-09-17 21:51:36 +0530

    Added solution - LeetHub

--- 704184-count-squares-GFG/704184-count-squares.java
+++ 704184-count-squares-GFG/704184-count-squares.java
@@ -3,13 +3,19 @@
 class Solution {
     static int countSquares(int n) {
         // code here
-        int i=1;
-        int count=0;
-        while(i*i<n){
-            count++;
-            i++;
-        }
+        // int i=1;
+        // int count=0;
+        // while(i*i<n){
+        //     count++;
+        //     i++;
+        // }
         
+        // return count;
+        
+        int count=0;
+        for(int i =1; i*i <n; i++){
+            count++;
+        }
         return count;
     }
 }
