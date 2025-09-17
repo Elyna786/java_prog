8f7716046df4cdc9a3d2add407b348198c85aaa3
Elyna786 <135322645+Elyna786@users.noreply.github.com>
2025-09-17 20:50:56 +0530

    Added solution - LeetHub

---
+++ 703715-addition-of-two-numbers-GFG/703715-addition-of-two-numbers.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+class Solution {
+    static int addition(int a, int b) {
+        // code here
+        int sum = 0;
+        sum = a+b;
+        return sum;
+    }
+    
+}
