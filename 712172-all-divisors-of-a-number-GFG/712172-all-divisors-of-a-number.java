89a1924a7ab5cccdd5ba5c2ab06b838872d438a6
Elyna786 <135322645+Elyna786@users.noreply.github.com>
2025-09-18 21:23:38 +0530

    Added solution - LeetHub

--- 712172-all-divisors-of-a-number-GFG/712172-all-divisors-of-a-number.java
+++ 712172-all-divisors-of-a-number-GFG/712172-all-divisors-of-a-number.java
@@ -1,19 +1,32 @@
 class Solution {
     public static void print_divisors(int n) {
         // code here
-        ArrayList<Integer> divisor = new ArrayList<>();
+        // ArrayList<Integer> divisor = new ArrayList<>();
 
+        // for(int i=1; i*i<=n;i++){
+        //     if(n%i==0){
+        //         divisor.add(i);
+        //         if(i != n/i) divisor.add(n/i);
+        //     }
+        // }
+        
+        // Collections.sort(divisor);
+        
+        // for(int d:divisor){
+        //     System.out.print(d + " ");
+        // }
+        
         for(int i=1; i*i<=n;i++){
             if(n%i==0){
-                divisor.add(i);
-                if(i != n/i) divisor.add(n/i);
+                System.out.print(i + " ");
             }
         }
         
-        Collections.sort(divisor);
-        
-        for(int d:divisor){
-            System.out.print(d + " ");
+        int sq = (int) Math.sqrt(n);
+        for(int j=sq; j>0;j--){
+            if(n%j==0 && j != n/j){
+                System.out.print(n/j + " ");
+            }
         }
     }
 }
