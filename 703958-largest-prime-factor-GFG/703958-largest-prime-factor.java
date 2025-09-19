0744a2326ecf8392d8413c3b395e2d369077276c
Elyna786 <135322645+Elyna786@users.noreply.github.com>
2025-09-19 21:12:24 +0530

    Added solution - LeetHub

--- 703958-largest-prime-factor-GFG/703958-largest-prime-factor.java
+++ 703958-largest-prime-factor-GFG/703958-largest-prime-factor.java
@@ -3,29 +3,56 @@
 class Solution {
     static int largestPrimeFactor(int n) {
         
-        if (n<=1) return 0;
-        
-        while((n & 1) == 0){
-            n >>= 1;
-        }
-        
-        int preprime = 2, nextprime = 3;
-        
-        while(nextprime <= n){
-            if(n%nextprime==0){
-                n/=nextprime;
-                preprime=nextprime;
-                while(n%nextprime==0){
-                    n/=nextprime;
+        if (n <= 1) return -1;
+
+			// Remove all 2s
+			while ((n & 1) == 0) {
+				n >>= 1;
+			}
+
+        int factor = 3;
+        int lastFactor = 2;
+
+        while (factor * factor <= n) {
+            if (n % factor == 0) {
+                n /= factor;
+                lastFactor = factor;
+                while (n % factor == 0) {
+                    n /= factor;
                 }
             }
-            nextprime+=2;
+            factor += 2;
         }
-        
-        if(n>1){
+
+        if (n > 1) {
             return n;
         }
+
+        return lastFactor;
         
-        return preprime;
+        // if (n<=1) return 0;
+        
+        // while((n & 1) == 0){
+        //     n >>= 1;
+        // }
+        
+        // int preprime = 2, nextprime = 3;
+        
+        // while(nextprime <= n){
+        //     if(n%nextprime==0){
+        //         n/=nextprime;
+        //         preprime=nextprime;
+        //         while(n%nextprime==0){
+        //             n/=nextprime;
+        //         }
+        //     }
+        //     nextprime+=2;
+        // }
+        
+        // if(n>1){
+        //     return n;
+        // }
+        
+        // return preprime;
     }
 }
