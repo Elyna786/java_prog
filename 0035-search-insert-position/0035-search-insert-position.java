750906dc1ff726d0d963440784da4e002cb72b18
Elyna786 <135322645+Elyna786@users.noreply.github.com>
2025-09-22 21:32:10 +0530

    Time: 0 ms (100%), Space: 43 MB (66.63%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,15 +1,12 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int st=0, end=nums.length-1;
-        int ans = bs(nums, target, st, end);
-        return ans;        
+        int st = 0, end = nums.length-1;
+        return bs(nums, target, st, end);
     }
 
-    public int bs(int[] arr, int tar, int st, int end){
-        int mid=st+(end-st)/2;
+    public int bs(int[] arr, int tar, int st, int end){       
         if(st<=end){
-            
-
+            int mid=st+(end-st)/2;
             if(tar>arr[mid]){
                 return bs(arr, tar, mid+1, end);
             } else if(tar<arr[mid]){
@@ -18,6 +15,6 @@ class Solution {
                 return mid;
             }
         }
-        return mid;
+        return st;
     }
 }
