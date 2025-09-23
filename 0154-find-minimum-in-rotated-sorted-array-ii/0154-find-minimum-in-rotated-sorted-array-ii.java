8e5fba8cabd6061aec04edbac0c9c96b7bc097fd
Elyna786 <135322645+Elyna786@users.noreply.github.com>
2025-09-23 23:03:58 +0530

    Time: 0 ms (100%), Space: 43.6 MB (13.85%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -15,5 +15,21 @@ class Solution {
             }
         }
         return nums[st];
+
+
+
+        // int st =0, end = nums.length-1;
+
+        // while(st<end){
+        //     int mid=st+(end-st)/2;
+        //     if(nums[mid] > nums[end]){
+        //         st = mid+1;
+        //     } else if(nums[mid] < nums[end]){
+        //         end = mid;
+        //     } else {
+        //         end--;
+        //     }
+        // }
+        // return nums[st];
     }
 }
