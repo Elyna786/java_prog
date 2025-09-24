1ceb317854490d41b4236e9abeb3b4081366ddbf
Elyna786 <135322645+Elyna786@users.noreply.github.com>
2025-09-24 17:34:03 +0530

    Time: 0 ms (100%), Space: 50.5 MB (31.03%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -17,6 +17,19 @@ class Solution {
                 }
             }
         }
-        return nums[st];        
+        return nums[st];       
+
+        //different approach
+        // int st=0, end=nums.length-1;
+        // while(st<end){
+        //     int mid=st+(end-st)/2;
+        //     if(mid%2==1) mid--;
+        //     if(nums[mid] == nums[mid+1]) {
+        //         st = mid + 2;
+        //     } else {
+        //         end=mid;
+        //     }
+        // }
+        // return nums[st]; 
     }
 }
